c27364ee45669fa826beb511294745d6f17b310e
Valep42 <p.czempin@gmx.de>
2014-02-27 19:24:36 +0100

    changed Mouse controls

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -16,9 +16,10 @@ import engine.gui.GUI;
 import engine.gui.StaticText;
 
 public class Game extends BasicGame {
-	private List <Drawable>drawables;
-	private List <GUI>guiElements;
-	private boolean showFPS = false;
+	private List<Drawable> drawables;
+	private List<GUI> guiElements;
+	private boolean showFPS;
+	private boolean mouseWasClicked;
 	Button button1, button2;
 
 	public Game() {
@@ -27,24 +28,22 @@ public class Game extends BasicGame {
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		this.drawables = new  ArrayList<Drawable>();
-		this.guiElements = new  ArrayList<GUI>();
-		
-		
+		this.drawables = new ArrayList<Drawable>();
+		this.guiElements = new ArrayList<GUI>();
+		this.mouseWasClicked = false;
+		this.showFPS = false;
+
 		button1 = new Button(300, 300, "./data/button1.png", "./data/button2.png");
 		button2 = new Button(200, 300, "./data/button1.png", "./data/button2.png");
-		
-		
-		// add all objects that need to be drawn to the respectable arrays 
-		//entities
+
+		// add all objects that need to be drawn to the respectable arrays
+		// entities
 		this.drawables.add(new TestEntity(10, 10, 180, "./data/A.bmp"));
-		
-		//GUI
+
+		// GUI
 		this.guiElements.add(this.button1);
 		this.guiElements.add(this.button2);
 		this.guiElements.add(new StaticText(100, 100, 10, Color.green, "Hello World"));
-		
-		
 
 		container.setShowFPS(this.showFPS);
 
@@ -52,8 +51,7 @@ public class Game extends BasicGame {
 
 	@Override
 	public void render(GameContainer container, Graphics graphics) throws SlickException {
-		
-		
+
 		for (Drawable entity : this.drawables) {
 			entity.draw();
 		}
@@ -65,23 +63,33 @@ public class Game extends BasicGame {
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
+
+		this.mouseEvents(container, delta);
+
+	}
+
+	private void mouseEvents(GameContainer container, int delta) {
 		Input input = container.getInput();
 		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+		
 			float x = input.getMouseX();
 			float y = input.getMouseY();
 			if (button1.checkCollision(x, y)) {
 				button1.onClick();
 			}
+			if (button2.checkCollision(x, y)) {
+				button2.onClick();
+			}
+			this.mouseWasClicked = true;
 
 		}
-		if (!input.isMouseButtonDown(0)) {
+		if (this.mouseWasClicked && !input.isMouseButtonDown(0)) {
+			
+			this.mouseWasClicked = false;
+			
 			button1.onRelease();
+			button2.onRelease();
 		}
-
-		// if(Mouse.isButtonDown(0)) {
-		// System.out.println(Mouse.getDX() + ", " + Mouse.getDY());
-		// }
-
 	}
 
 	public static void main(String[] args) {
