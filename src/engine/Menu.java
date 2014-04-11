5b39d53c7032304c299fd9a7b3a465696fc04015
Valep42 <p.czempin@gmx.de>
2014-04-11 18:01:34 +0200

    some tests with text fields

--- src/engine/Menu.java
+++ src/engine/Menu.java
@@ -1,13 +1,20 @@
 package engine;
 
+import java.awt.Font;
+
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.gui.TextField;
 
 import engine.gui.ExitClickable;
 import engine.gui.StartClickable;
 
 public class Menu extends GameComponent {
+	private TextField t;
 
 	public Menu(TowerDefense game) {
 		super(game);
@@ -23,18 +30,28 @@ public class Menu extends GameComponent {
 		ExitClickable e = new ExitClickable(0, 20, this.game);
 		this.clickables.add(e);
 		this.guiElements.add(e);
+
+		this.t = new TextField(container, new TrueTypeFont(new Font("Verdana", Font.PLAIN, 15), true), 0, 100, 75, 25);
+		this.t.setText("Player 1");
+		this.t.setBorderColor(Color.gray);
+		this.t.setBackgroundColor(Color.lightGray);
+		this.t.setMaxLength(32);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
 		super.update(container, delta);
+		Input input = container.getInput();
+		if (input.isKeyPressed(Input.KEY_ENTER)) {
+			this.t.deactivate();
+		}
 
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics graphics) throws SlickException {
 		super.render(container, graphics);
-
+		this.t.render(container, graphics);
 	}
 
 }
