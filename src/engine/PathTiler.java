73328ccfe87af342bcac719cbcb189cee6586983
Valep42 <p.czempin@gmx.de>
2014-04-11 16:02:09 +0200

    changed path tiler

--- src/engine/PathTiler.java
+++ src/engine/PathTiler.java
@@ -6,7 +6,7 @@ public class PathTiler {
 	private Sprite corner1, corner2, corner3, corner4, horizontal, vertical, start, end;
 	private Sprite[][] path;
 
-	public PathTiler(int width, int height, Waypoint waypoint) {
+	public PathTiler(int[][] path1) {
 		this.corner1 = new Sprite("VeinCorner.png");
 		this.corner2 = new Sprite("VeinCorner2.png");
 		this.corner3 = new Sprite("VeinCorner3.png");
@@ -15,5 +15,62 @@ public class PathTiler {
 		this.vertical = new Sprite("VeinVert.png");
 		this.start = new Sprite("VeinVertStart.png");
 		this.end = new Sprite("VeinVertEnd.png");
+		this.path = new Sprite[path1.length][path1[0].length];
+
+		// Waypoint currentWaypoint = waypoint;
+		//
+		// int x = (int) (currentWaypoint.getX() / 50);
+		// int y = (int) (currentWaypoint.getY() / 50);
+		// this.path[x][y] = this.start;
+		// if(currentWaypoint.getDirection() == )
+		for (int y = 0; y < path1.length; ++y) {
+			for (int x = 0; x < path1[0].length; ++x) {
+				if (path1[y][x] == 0) {
+					boolean above = false, below = false, left = false, right = false;
+					if (path1[y - 1][x] == 0) {
+						above = true;
+					}
+					if (path1[y + 1][x] == 0) {
+						below = true;
+					}
+					if (path1[y][x - 1] == 0) {
+						left = true;
+					}
+					if (path1[y][x + 1] == 0) {
+						right = true;
+					}
+
+					if (below && !above && !right && !left) {
+						this.path[y][x] = this.start;
+					} else if (!below && above && !right && !left) {
+						this.path[y][x] = this.end;
+					} else if (below && above && !right && !left) {
+						this.path[y][x] = this.vertical;
+					} else if (!below && !above && right && left) {
+						this.path[y][x] = this.horizontal;
+					} else if (!below && above && right && !left) {
+						this.path[y][x] = this.corner1;
+					} else if (below && !above && right && !left) {
+						this.path[y][x] = this.corner2;
+					} else if (below && !above && !right && left) {
+						this.path[y][x] = this.corner3;
+					} else if (!below && above && !right && left) {
+						this.path[y][x] = this.vertical;
+					}
+
+				}
+			}
+		}
+
+	}
+
+	public void render() {
+		for (int y = 0; y < this.path.length; ++y) {
+			for (int x = 0; x < this.path[0].length; ++x) {
+				if (this.path[y][x] != null) {
+					this.path[y][x].draw(x * 50, y * 50);
+				}
+			}
+		}
 	}
 }
