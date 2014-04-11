4174e054d696748c723b22848306e28418c08bc2
Valep42 <p.czempin@gmx.de>
2014-04-11 14:36:12 +0200

    changed data path

--- src/engine/MapLayoutFromImage.java
+++ src/engine/MapLayoutFromImage.java
@@ -29,7 +29,7 @@ public class MapLayoutFromImage {
 	public MapLayoutFromImage(String imagePath) {
 
 		try {
-			this.image = new Image("./data/" + imagePath);
+			this.image = new Image("data/" + imagePath);
 		} catch (SlickException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
