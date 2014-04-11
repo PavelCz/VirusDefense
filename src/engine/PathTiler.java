d56e20a5012c094de1154e9e275465b250d340fc
Valep42 <p.czempin@gmx.de>
2014-04-11 18:07:22 +0200

    renaming

--- src/engine/PathTiler.java
+++ src/engine/PathTiler.java
@@ -4,9 +4,9 @@ import engine.graphics.Sprite;
 
 public class PathTiler {
 	private Sprite corner1, corner2, corner3, corner4, horizontal, vertical, top, bottom, left, right;
-	private Sprite[][] path;
+	private Sprite[][] tiles;
 
-	public PathTiler(int[][] path1) {
+	public PathTiler(int[][] path) {
 		this.corner1 = new Sprite("veins/VeinCorner.png");
 		this.corner2 = new Sprite("veins/VeinCorner2.png");
 		this.corner3 = new Sprite("veins/VeinCorner3.png");
@@ -17,45 +17,45 @@ public class PathTiler {
 		this.bottom = new Sprite("veins/VeinBottomEnd.png");
 		this.left = new Sprite("veins/VeinLeftEnd.png");
 		this.right = new Sprite("veins/VeinRightEnd.png");
-		this.path = new Sprite[path1.length][path1[0].length];
+		this.tiles = new Sprite[path.length][path[0].length];
 
-		for (int y = 0; y < path1.length; ++y) {
-			for (int x = 0; x < path1[0].length; ++x) {
-				if (path1[y][x] == 5) {
+		for (int y = 0; y < path.length; ++y) {
+			for (int x = 0; x < path[0].length; ++x) {
+				if (path[y][x] == 5) {
 					boolean above = false, below = false, left = false, right = false;
-					if (y - 1 >= 0 && path1[y - 1][x] == 5) {
+					if (y - 1 >= 0 && path[y - 1][x] == 5) {
 						above = true;
 					}
-					if (y + 1 < path1.length && path1[y + 1][x] == 5) {
+					if (y + 1 < path.length && path[y + 1][x] == 5) {
 						below = true;
 					}
-					if (x - 1 >= 0 && path1[y][x - 1] == 5) {
+					if (x - 1 >= 0 && path[y][x - 1] == 5) {
 						left = true;
 					}
-					if (x + 1 < path1[0].length && path1[y][x + 1] == 5) {
+					if (x + 1 < path[0].length && path[y][x + 1] == 5) {
 						right = true;
 					}
 
 					if (below && !above && !right && !left) {
-						this.path[y][x] = this.top;
+						this.tiles[y][x] = this.top;
 					} else if (!below && above && !right && !left) {
-						this.path[y][x] = this.bottom;
+						this.tiles[y][x] = this.bottom;
 					} else if (!below && !above && right && !left) {
-						this.path[y][x] = this.left;
+						this.tiles[y][x] = this.left;
 					} else if (!below && !above && !right && left) {
-						this.path[y][x] = this.right;
+						this.tiles[y][x] = this.right;
 					} else if (below && above && !right && !left) {
-						this.path[y][x] = this.vertical;
+						this.tiles[y][x] = this.vertical;
 					} else if (!below && !above && right && left) {
-						this.path[y][x] = this.horizontal;
+						this.tiles[y][x] = this.horizontal;
 					} else if (!below && above && right && !left) {
-						this.path[y][x] = this.corner1;
+						this.tiles[y][x] = this.corner1;
 					} else if (below && !above && right && !left) {
-						this.path[y][x] = this.corner2;
+						this.tiles[y][x] = this.corner2;
 					} else if (below && !above && !right && left) {
-						this.path[y][x] = this.corner3;
+						this.tiles[y][x] = this.corner3;
 					} else if (!below && above && !right && left) {
-						this.path[y][x] = this.corner4;
+						this.tiles[y][x] = this.corner4;
 					}
 
 				}
@@ -65,10 +65,10 @@ public class PathTiler {
 	}
 
 	public void render() {
-		for (int y = 0; y < this.path.length; ++y) {
-			for (int x = 0; x < this.path[0].length; ++x) {
-				if (this.path[y][x] != null) {
-					this.path[y][x].draw(x * 50, y * 50);
+		for (int y = 0; y < this.tiles.length; ++y) {
+			for (int x = 0; x < this.tiles[0].length; ++x) {
+				if (this.tiles[y][x] != null) {
+					this.tiles[y][x].draw(x * 50, y * 50);
 				}
 			}
 		}
