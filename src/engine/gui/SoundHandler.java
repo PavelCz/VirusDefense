0e4589170f1d433a0c16370b53398eebeacebcf3
Pavel Czempin <valep.pave@gmx.de>
2014-05-04 13:45:27 +0200

    added method play() to SoundHandler

--- src/engine/gui/SoundHandler.java
+++ src/engine/gui/SoundHandler.java
@@ -1,16 +1,14 @@
 package engine.gui;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
 public class SoundHandler {
-	private Map<String,Sound> sounds = new HashMap<String,Sound>();
-	
+	private Map<String, Sound> sounds = new HashMap<String, Sound>();
+
 	public void add(String name, String location) {
 		try {
 			this.sounds.put(name, new Sound(location));
@@ -19,6 +17,11 @@ public class SoundHandler {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	public void play(String name) {
+		if (this.sounds.containsKey(name)) {
+			this.sounds.get(name).play();
+		}
+	}
+
 }
