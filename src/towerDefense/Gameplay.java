5150efcd231cbe4157a07245f1004f83155eaf35
Valep42 <p.czempin@gmx.de>
2014-05-10 00:01:07 +0200

    NOW it should work

--- src/towerDefense/Gameplay.java
+++ src/towerDefense/Gameplay.java
@@ -105,7 +105,8 @@ public class Gameplay extends GameComponent {
 		Gameplay.INTERFACE_START_X = TowerDefense.getWidth() - 3 * 64 * Gameplay.GLOBAL_GUI_SCALE;
 		float scale1 = Gameplay.INTERFACE_START_X / this.width;
 		float scale2 = TowerDefense.getHeight() / this.height;
-		Gameplay.SIZE = (int) Math.min(scale1, scale2);
+		Gameplay.GLOBAL_GAME_SCALE = Math.min(scale1, scale2);
+		Gameplay.SIZE = (int) (64 * Gameplay.GLOBAL_GAME_SCALE);
 
 		this.mapBackground = new BackgroundTiles(0.5f, "veins/bg.png", this.getHorizontalTiles(), this.getVerticalTiles());
 		//
