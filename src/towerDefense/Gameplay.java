77c2e128d41a38b70ac8f9d3e7380b92f901ef6f
Pavel Czempin <valep.pave@gmx.de>
2014-05-17 18:08:27 +0200

    checks now should also work with zoomed view

--- src/towerDefense/Gameplay.java
+++ src/towerDefense/Gameplay.java
@@ -402,8 +402,8 @@ public class Gameplay extends GameComponent {
 			}
 		}
 		if (input.isKeyPressed(Input.KEY_RIGHT)) {
-			if (Gameplay.getCameraX() + this.getHorizontalTiles() * Gameplay.DEFAULT_SIZE < this.getHorizontalTiles()
-					* Gameplay.DEFAULT_SIZE) {
+			if (Gameplay.getCameraX() + this.getHorizontalTiles() * Gameplay.SIZE < this.getHorizontalTiles()
+					* Gameplay.SIZE) {
 				Gameplay.camera.addX(+32);
 			}
 		}
@@ -413,8 +413,8 @@ public class Gameplay extends GameComponent {
 			}
 		}
 		if (input.isKeyPressed(Input.KEY_DOWN)) {
-			if (Gameplay.getCameraY() + this.getHorizontalTiles() * Gameplay.DEFAULT_SIZE < this.getVerticalTiles()
-					* Gameplay.DEFAULT_SIZE) {
+			if (Gameplay.getCameraY() + this.getHorizontalTiles() * Gameplay.SIZE < this.getVerticalTiles()
+					* Gameplay.SIZE) {
 				Gameplay.camera.addY(+32);
 			}
 		}
