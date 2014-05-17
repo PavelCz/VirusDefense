9ed2a99d0e7feb3268e6cf41289ef29a09b31fd5
Pavel Czempin <valep.pave@gmx.de>
2014-05-17 19:35:22 +0200

    better position of enemy radius debug

--- src/towerDefense/Gameplay.java
+++ src/towerDefense/Gameplay.java
@@ -272,9 +272,9 @@ public class Gameplay extends GameComponent {
 	private void renderDebug(GameContainer container, Graphics graphics) {
 		if (this.debugMode) {
 			for (Enemy enemy : this.enemies) {
-				new SlickUnfilledEllipse(graphics, enemy.getRadius() * 2, enemy.getRadius() * 2, Color.blue).draw(enemy.getX()
-						- Gameplay.getCameraX() * Gameplay.CURRENT_GAME_SCALE, enemy.getY() - Gameplay.getCameraY()
-						* Gameplay.CURRENT_GAME_SCALE, Gameplay.CURRENT_GAME_SCALE);
+				new SlickUnfilledEllipse(graphics, enemy.getRadius() * 2, enemy.getRadius() * 2, Color.blue).draw(
+						(enemy.getX() ) * Gameplay.CURRENT_GAME_SCALE- Gameplay.getCameraX(), (enemy.getY() )
+								* Gameplay.CURRENT_GAME_SCALE- Gameplay.getCameraY(), Gameplay.CURRENT_GAME_SCALE);
 			}
 			for (int i = 0; i < this.towers.length; ++i) {
 				for (int j = 0; j < this.towers[0].length; ++j) {
