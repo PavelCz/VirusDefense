65f6e2aabb52e4f76dbde45536b0e53501bf5e3c
Valep42 <p.czempin@gmx.de>
2014-05-28 22:31:56 +0200

    added back button

--- src/towerDefense/Settings.java
+++ src/towerDefense/Settings.java
@@ -1,12 +1,18 @@
 package towerDefense;
 
 import engine.GameComponent;
+import engine.gui.GoToMenuButton;
 
 public class Settings extends GameComponent {
 
 	public Settings(TowerDefense game) {
 		super(game);
 
+		GoToMenuButton settings = new GoToMenuButton(0, 0, "Back", this.game);
+		this.clickables.add(settings);
+		this.guiElements.add(settings);
+		settings.setX(0);
+		settings.setY(TowerDefense.getHeight() - settings.getHeight() * 2);
 	}
 
 }
