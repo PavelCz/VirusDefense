b526847dac04d4170be8a1b501d63cedd6dde006
Valep42 <p.czempin@gmx.de>
2014-05-28 22:19:34 +0200

    added basic onRelease() mehtod to settings button

--- src/engine/gui/GoToSettingsButton.java
+++ src/engine/gui/GoToSettingsButton.java
@@ -4,9 +4,17 @@ import towerDefense.Gameplay;
 import towerDefense.TowerDefense;
 
 public class GoToSettingsButton extends ClickableText {
+	private TowerDefense towerDefense;
 
 	public GoToSettingsButton(float x, float y, String text, TowerDefense towerDefense) {
 		super(x, y, text, Gameplay.GLOBAL_GUI_SCALE, towerDefense.getGameplay(), false);
+		this.towerDefense = towerDefense;
+
+	}
+
+	@Override
+	public void onRelease() {
+		super.onRelease();
 	}
 
 }
