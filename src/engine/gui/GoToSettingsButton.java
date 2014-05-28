c9ee6a40ab81a53c4c7f246e2fa8a1f923993c8b
Valep42 <p.czempin@gmx.de>
2014-05-28 22:17:42 +0200

    changed settings button arguments

--- src/engine/gui/GoToSettingsButton.java
+++ src/engine/gui/GoToSettingsButton.java
@@ -1,11 +1,12 @@
 package engine.gui;
 
 import towerDefense.Gameplay;
+import towerDefense.TowerDefense;
 
 public class GoToSettingsButton extends ClickableText {
 
-	public GoToSettingsButton(float x, float y, String text, Gameplay game) {
-		super(x, y, text, Gameplay.GLOBAL_GUI_SCALE, game, false);
+	public GoToSettingsButton(float x, float y, String text, TowerDefense towerDefense) {
+		super(x, y, text, Gameplay.GLOBAL_GUI_SCALE, towerDefense.getGameplay(), false);
 	}
 
 }
