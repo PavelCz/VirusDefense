72b2f00442bc0ede501c74e36c415941e9d6511d
Valep42 <p.czempin@gmx.de>
2014-05-30 17:06:23 +0200

    added AnimatedSprite

---
+++ src/engine/graphics/AnimatedSprite.java
@@ -0,0 +1,36 @@
+package engine.graphics;
+
+import java.util.List;
+
+public class AnimatedSprite extends RenderObject {
+	private List<Sprite> sprites;
+	private int speed;
+	private int index;
+	private Sprite currentSprite;
+
+	public AnimatedSprite(String[] images, float scale, int speed) {
+
+		for (String string : images) {
+			this.sprites.add(new Sprite(string, scale));
+
+		}
+		this.speed = speed;
+		this.index = 0;
+		this.currentSprite = this.sprites.get(0);
+	}
+
+	public void updateAnimation(int delta) {
+		this.speed -= delta;
+		if (this.speed <= 0) {
+			this.index += 1;
+			this.index %= this.sprites.size();
+			this.currentSprite = this.sprites.get(this.index);
+		}
+	}
+
+	@Override
+	public void draw(float x, float y, float globalScale) {
+		this.currentSprite.draw(x, y, globalScale);
+
+	}
+}
