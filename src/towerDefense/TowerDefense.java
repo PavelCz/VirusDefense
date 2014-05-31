8ab89e1bcc9b7c4d362117f73affd7477acf4fc4
Valep42 <p.czempin@gmx.de>
2014-05-31 22:13:17 +0200

    icon now correct size

--- src/towerDefense/TowerDefense.java
+++ src/towerDefense/TowerDefense.java
@@ -47,9 +47,8 @@ public class TowerDefense extends BasicGame {
 
 	@Override
 	public void init(GameContainer container) {
-		if (!container.isFullscreen()) {
-			String[] icons = { "./data/graphics/icons/icon16.png", "./data/graphics/icons/icon24.png",
-					"./data/graphics/icons/icon32.png" };
+		if (!container.isFullscreen()) {/* "./data/graphics/icons/icon24.png", (this may be necessary for other platforms(mac)) */
+			String[] icons = { "./data/graphics/icons/icon16.png", "./data/graphics/icons/icon32.png" };
 			try {
 				container.setIcons(icons);
 			} catch (SlickException e) {
