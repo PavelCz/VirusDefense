f0d64c9366d6e23147facb3fb8e6b322ff7cb718
Valep42 <p.czempin@gmx.de>
2014-05-31 19:14:23 +0200

    better back button position

--- src/towerDefense/Scores.java
+++ src/towerDefense/Scores.java
@@ -8,7 +8,7 @@ public class Scores extends GameComponent {
 	public Scores(TowerDefense game) {
 		super(game);
 
-		GoToMenuButton back = new GoToMenuButton(0, TowerDefense.getHeight() - Gameplay.STANDARD_TEXT_SCALE, "Back", this.game);
+		GoToMenuButton back = new GoToMenuButton(0, TowerDefense.getHeight() - 20, "Back", this.game);
 		this.clickables.add(back);
 		this.guiElements.add(back);
 
