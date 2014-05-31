e9e66b29094bdce0175a019f574fd68ddcfb5824
Valep42 <p.czempin@gmx.de>
2014-05-31 19:01:13 +0200

    added back button to score screen

--- src/towerDefense/Scores.java
+++ src/towerDefense/Scores.java
@@ -1,11 +1,16 @@
 package towerDefense;
 
 import engine.GameComponent;
+import engine.gui.GoToMenuButton;
 
 public class Scores extends GameComponent {
 
 	public Scores(TowerDefense game) {
 		super(game);
+
+		GoToMenuButton back = new GoToMenuButton(0, TowerDefense.getHeight() - Gameplay.STANDARD_TEXT_SCALE, "Back", this.game);
+		this.clickables.add(back);
+		this.guiElements.add(back);
 
 	}
 
